707a26451f6a3b1f91ae6a84ec34a9663bd2b91a
ilya-vasilyev <ilya.vasilyev@gmx.com>
2022-08-09 17:54:35 +0300

    2 files modified, 1 added

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,10 +1,27 @@
 <template>
-  <div data-theme="emerald" class="min-h-screen">
+  <div data-theme="cupcake" class="min-h-screen">
     <div class="max-w-2xl mx-auto px-4 py-2 prose">
-      <nav class="flex gap-4">
+      <!-- <nav class="flex gap-4">
         <router-link to="/">Home</router-link>
         <router-link to="/about">About</router-link>
-      </nav>
+      </nav> -->
+
+      <div class="navbar bg-base-100">
+        <div class="flex-1">
+          <a class="btn btn-ghost normal-case text-xl">LOGO</a>
+        </div>
+        <div class="flex-none">
+          <ul class="menu menu-horizontal p-0">
+            <li><router-link to="/">Home</router-link></li>
+            <li><router-link to="/about">About</router-link></li>
+            <li>
+              <button class="btn btn-primary">
+                <router-link to="/subscribe">subscribe</router-link>
+              </button>
+            </li>
+          </ul>
+        </div>
+      </div>
 
       <!-- PAGE CONTENT -->
       <slot />
